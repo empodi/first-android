257204c8d39adaa69c2bad774041d57239e9797d
empodi <kglein9513@gmail.com>
2023-12-13 17:24:49 +0900

    Update ApiService.java

--- src/main/java/com/example/myapp/api/ApiService.java
+++ src/main/java/com/example/myapp/api/ApiService.java
@@ -1,15 +1,31 @@
 package com.example.myapp.api;
 
-import com.example.myapp.dto.LoginRequest;
+import com.example.myapp.UserIdCheckResponse;
+import com.example.myapp.dto.HaniItem;
+import com.example.myapp.dto.userRequest;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 public interface ApiService {
 
     @POST("user/login")
-    Call<Void> loginUser(@Body LoginRequest loginRequest);
+    Call<Void> loginUser(@Body userRequest userRequest);
 
+    @POST("user/signup")
+    Call<Void> signUpUser(@Body userRequest userRequest);
 
+    @POST("/hani/update") // Replace with your actual endpoint
+    Call<Void> postHaniItems(@Body List<HaniItem> haniItems);
+
+    @GET("/hani/all")
+    Call<List<HaniItem>> getAllHaniData();
+
+    @GET("/user/check")
+    Call<UserIdCheckResponse> checkUserId(@Query("userId") String userId);
 }
